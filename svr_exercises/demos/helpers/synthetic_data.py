0c8a2bd1b9786e9fe24d62b6c54d99b6ba2752c3
gddeazevedo <gabrielcapuerj@gmail.com>
2025-06-25 22:07:48 -0300

    adicionando exemplo

---
+++ svr_exercises/demos/helpers/synthetic_data.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+def generate_linear_data(
+        ang_coef: float,
+        linear_coef: float,
+        n_samples: int,
+        with_errors: bool = True
+    ) -> tuple[np.ndarray, np.ndarray]:
+    '''
+    Generate linear data with or without errors
+    '''
+    np.random.seed(42)
+    X = np.linspace(0, 10, n_samples).reshape(-1, 1) # shape = (100, 1)
+
+    errors = 0
+
+    if with_errors:
+        errors = np.random.normal(0, 2, n_samples)
+
+    y = ang_coef * X.squeeze() + linear_coef + errors
+
+    return X, y
